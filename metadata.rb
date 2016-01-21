796eee5c183d44598537974de06f4e872f32469d
9ab62cf36bcecb2f07d0b948fa3ce74288a6697d <keli.grubb@spinen.com>
2016-01-21 10:24:39 -0500

    updating version & triggering travis build

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'svargo@customink.com'
 license          'All rights reserved'
 description      'Installs/Configures tmpreaper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 recipe 'tmpreaper', 'Main tmpreaper configuration'
 recipe 'tmpreaper::schedule', 'Adds cron job for tmpreaper'
